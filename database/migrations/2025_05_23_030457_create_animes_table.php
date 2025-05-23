f4e20042f537b6c4ec4117bfc6ed3238e91790e6
massan02 <107457475+massan02@users.noreply.github.com>
2025-05-23 03:07:24 +0000

    マイグレーション課題　完了

---
+++ database/migrations/2025_05_23_030457_create_animes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('genre');
+            $table->integer('episodes');
+            $table->date('aired_from')->nullable();
+            $table->date('aired_to')->nullable();
+            $table->decimal('rating', 3, 1)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animes');
+    }
+}
